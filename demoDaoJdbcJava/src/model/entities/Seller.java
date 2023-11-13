3c250283ac259414e523a55881bb8e66f7b899c1
Felipe Gabriel <felipegabrielodss@gmail.com>
2023-11-13 18:54:41 -0300

    Creating project and git repository (create class seller, department and create interfaces DepartmentDao and SellerDao)

---
+++ demoDaoJdbcJava/src/model/entities/Seller.java
@@ -0,0 +1,103 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+public class Seller implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String emaill;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	private Department department; 
+	
+	public Seller() {
+	}
+
+	public Seller(Integer id, String name, String emaill, Date birthDate, Double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.emaill = emaill;
+		this.birthDate = birthDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmaill() {
+		return emaill;
+	}
+
+	public void setEmaill(String emaill) {
+		this.emaill = emaill;
+	}
+
+	public Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(Double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", emaill=" + emaill + ", birthDate=" + birthDate
+				+ ", baseSalary=" + baseSalary + ", department=" + department + "]";
+	}
+	
+	
+}
+
